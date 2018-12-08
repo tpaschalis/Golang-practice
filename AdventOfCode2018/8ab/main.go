8864a35936074f5f8b6c09e9e4646417cdba8597
TPaschalis <paschalist0@gmail.com>
2018-12-09 01:58:27 +0200

    Advent of Code 2018 - Day 8 in Go

--- AdventOfCode2018/8ab/main.go
+++ AdventOfCode2018/8ab/main.go
@@ -10,6 +10,11 @@ import (
 
 var Metadata []int
 
+type node struct {
+	children []*node
+	metadata []int
+}
+
 func main() {
 	var input string
 	scanner := bufio.NewScanner(os.Stdin)
@@ -23,23 +28,47 @@ func main() {
 		curr, _ := strconv.Atoi(in[i])
 		tree = append(tree, curr)
 	}
-	_ = parse(tree, Metadata)
+	root, _ := parse(tree, Metadata)
 	fmt.Println(sumMetadata(Metadata))
+	fmt.Println(root.value())
+
 }
 
-func parse(data []int, metadata []int) int {
+func parse(data []int, metadata []int) (*node, int) {
 	idx := 2
+	n := &node{}
 
 	for i := data[0]; i > 0; i-- {
-		addMore := parse(data[idx:], metadata)
+		child, addMore := parse(data[idx:], metadata)
 		idx += addMore
+		n.children = append(n.children, child)
 	}
 	currMd := data[idx : idx+data[1]]
+	n.metadata = data[idx : idx+data[1]]
 	for i := range currMd {
 		Metadata = append(Metadata, currMd[i])
 	}
 	idx += data[1]
-	return idx
+	return n, idx
+}
+
+// For this part I cheated a bit :(
+// took a hint from github.com/Alex-Wauters
+func (n *node) value() (s int) {
+	if len(n.children) == 0 {
+		for _, meta := range n.metadata {
+			s += meta
+		}
+		return s
+	}
+	for _, meta := range n.metadata {
+		if meta > len(n.children) {
+			s += 0
+		} else {
+			s += n.children[meta-1].value()
+		}
+	}
+	return s
 }
 
 func sumMetadata(in []int) int {
